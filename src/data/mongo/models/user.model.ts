f1ff5e5980702f82277d11c24464da54862ef6c8
Andres Rivero <andres19rivero@gmail.com>
2024-01-05 12:48:31 -0400

    feat(models): implement user model

--- src/data/mongo/models/user.model.ts
+++ src/data/mongo/models/user.model.ts
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
     required: [true, "Email is required"],
     unique: true,
   },
+  emailValidated: {
+    type: Boolean,
+    unique: false,
+  },
   password: {
     type: String,
     required: [true, "Password is required"],
