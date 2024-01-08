7d26a174bbfb2f1a2df585b5aa60b2f3c73c00cf
Andres Rivero <andres19rivero@gmail.com>
2024-01-08 10:10:15 -0400

    feat: implement login functionality with loginUserDto

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -8,10 +8,10 @@ export class AuthRoutes {
     const authService = new AuthService();
     const controller = new AuthController(authService);
 
-    router.use("/login", controller.loginUser);
-    router.use("/register", controller.registerUser);
+    router.post("/login", controller.loginUser);
+    router.post("/register", controller.registerUser);
 
-    router.use("/validate-email/:token", controller.validateEmail);
+    router.get("/validate-email/:token", controller.validateEmail);
 
     return router;
   }
