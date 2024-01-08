ff9426fa29451cd5ca203206846517af33c4b4ed
Andres Rivero <andres19rivero@gmail.com>
2024-01-08 18:05:50 -0400

    feat: send email when create a user to valite token

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { JwtAdapter, bcryptAdapter } from "../../config";
+import { JwtAdapter, bcryptAdapter, envs } from "../../config";
 import { UserModel } from "../../data";
 import {
   CustomError,
@@ -6,9 +6,10 @@ import {
   RegisterUserDto,
   UserEntity,
 } from "../../domain";
+import { EmailService } from "./email.service";
 
 export class AuthService {
-  constructor() {}
+  constructor(private readonly emailService: EmailService) {}
 
   public async registerUser(registerUserDto: RegisterUserDto) {
     const existUser = await UserModel.findOne({ email: registerUserDto.email });
@@ -24,6 +25,7 @@ export class AuthService {
       // JWT - user authentication
 
       // Confirmation email
+      this.sendEmailValidationLink(user.email);
 
       const { password, ...userEntity } = UserEntity.fromObject(user);
       const token = await JwtAdapter.generateToken({ id: user.id });
@@ -59,4 +61,26 @@ export class AuthService {
       token,
     };
   }
+
+  private sendEmailValidationLink = async (email: string) => {
+    const token = await JwtAdapter.generateToken({ email });
+    if (!token) throw CustomError.internalServer("Error getting token");
+
+    const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+
+    const html = `
+      <h1>Validate your email</h1>
+      <p>Click on the following link to validate your email</p>
+      <a href="${link}">Validate your email: ${email}</a>
+    `;
+    const options = {
+      to: email,
+      subject: "Validate your email",
+      htmlBody: html,
+    };
+    const isSet = await this.emailService.sendEmail(options);
+    if (!isSet) throw CustomError.internalServer("Error sending email");
+
+    return true;
+  };
 }
