eaf71818eb2865f55d6d3c4fc78877d1bd210a71
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 09:26:28 -0400

    feat: implement routes & controllers for file upload api

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { CustomError } from "../../domain";
+
+export class FileUploadController {
+  constructor() {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+
+    console.log(`${error}`);
+    return res.status(500).json({ error: "Internal server error" });
+  };
+
+  uploadFile = async (req: Request, res: Response) => {
+    res.json("upload file");
+  };
+
+  uploadMultipleFiles = async (req: Request, res: Response) => {
+    res.json("upload multiple file");
+  };
+}
