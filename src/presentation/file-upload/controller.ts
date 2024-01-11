f54647d2051517ccf7bd446ef02811140d1ffb64
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 12:57:30 -0400

    feat: implement types middleware to validate file extensions

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -17,13 +17,8 @@ export class FileUploadController {
 
   uploadFile = async (req: Request, res: Response) => {
     const type = req.params.type;
-    const validTypes = ["users", "products", "categories"];
-    if (!validTypes.includes(type))
-      return res
-        .status(400)
-        .json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
-
     const file = req.body.files.at(0) as UploadedFile;
+
     this.fileUploadService
       .uploadSingle(file, `uploads/${type}`)
       .then((uploaded) => res.json(uploaded))
@@ -32,13 +27,8 @@ export class FileUploadController {
 
   uploadMultipleFiles = async (req: Request, res: Response) => {
     const type = req.params.type;
-    const validTypes = ["users", "products", "categories"];
-    if (!validTypes.includes(type))
-      return res
-        .status(400)
-        .json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
-
     const files = req.body.files as UploadedFile[];
+
     this.fileUploadService
       .uploadMultiple(files, `uploads/${type}`)
       .then((uploaded) => res.json(uploaded))
