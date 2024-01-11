42e50829812e3432ef5ef0eb1db16061037e8fb8
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 10:28:11 -0400

    feat(file-upload): implement service with method to upload a single file

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { CustomError } from "../../domain";
+import { FileUploadService } from "../services";
+import { UploadedFile } from "express-fileupload";
 
 export class FileUploadController {
-  constructor() {}
+  constructor(private readonly fileUploadService: FileUploadService) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError) {
@@ -14,7 +16,15 @@ export class FileUploadController {
   };
 
   uploadFile = async (req: Request, res: Response) => {
-    res.json("upload file");
+    const files = req.files;
+    if (!req.files || Object.keys(req.files).length === 0)
+      return res.status(400).json({ error: "No files where selected" });
+
+    const file = req.files.file as UploadedFile;
+    this.fileUploadService
+      .uploadSingle(file)
+      .then((uploaded) => res.json(uploaded))
+      .catch((error) => this.handleError(error, res));
   };
 
   uploadMultipleFiles = async (req: Request, res: Response) => {
--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { FileUploadController } from "./controller";
+import { FileUploadService } from "../services";
 
 export class FileUploadRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new FileUploadController();
+    const fileUploadService = new FileUploadService();
+    const controller = new FileUploadController(fileUploadService);
 
     router.post("/single/:type", controller.uploadFile);
     router.post("/multiple/:type", controller.uploadMultipleFiles);
