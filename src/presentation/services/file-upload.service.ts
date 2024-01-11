7faf0c4a280674d91f8d938963589825ce0f097e
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 11:21:42 -0400

    feat: upload files to sub folders

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -31,7 +31,7 @@ export class FileUploadService {
       file.mv(`${destination}/${fileName}`);
       return { fileName };
     } catch (error) {
-      console.log(error);
+      // console.log(error);
       throw error;
     }
   }
