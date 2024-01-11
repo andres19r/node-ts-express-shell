caabb752aaf80d779e08352897f129b78b9bb977
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 11:55:09 -0400

    feat(file-upload): implement & use middleware to verify files in req

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -23,10 +23,7 @@ export class FileUploadController {
         .status(400)
         .json({ error: `Invalid type: ${type}, valid ones ${validTypes}` });
 
-    if (!req.files || Object.keys(req.files).length === 0)
-      return res.status(400).json({ error: "No files where selected" });
-
-    const file = req.files.file as UploadedFile;
+    const file = req.body.files.at(0) as UploadedFile;
     this.fileUploadService
       .uploadSingle(file, `uploads/${type}`)
       .then((uploaded) => res.json(uploaded))
--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { FileUploadController } from "./controller";
 import { FileUploadService } from "../services";
+import { FileUploadMiddleware } from "../middlewares/file-upload.middleware";
 
 export class FileUploadRoutes {
   static get routes(): Router {
@@ -8,6 +9,7 @@ export class FileUploadRoutes {
     const fileUploadService = new FileUploadService();
     const controller = new FileUploadController(fileUploadService);
 
+    router.use(FileUploadMiddleware.containFiles);
     router.post("/single/:type", controller.uploadFile);
     router.post("/multiple/:type", controller.uploadMultipleFiles);
 
