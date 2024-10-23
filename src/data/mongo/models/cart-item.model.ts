71714b55455fc1a9123e02ff72cdb97359a14810
Andres Rivero <andres19rivero@gmail.com>
2024-10-22 21:00:12 -0400

    feat(cart-item): implement model

---
+++ src/data/mongo/models/cart-item.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  cart: {
+    type: Schema.Types.ObjectId,
+    ref: "Cart",
+    required: true,
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Quantity is required"],
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+cartItemSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret, options) {
+    delete ret._id;
+  },
+});
+
+export const CartModel = mongoose.model("CartItem", cartItemSchema);
