b8dffe4b78a0ba31f68df1e85f42fd3087d9f947
Andres Rivero <andres19rivero@gmail.com>
2024-10-24 23:54:15 -0400

    fix(product): add img field

--- src/data/mongo/models/product.model.ts
+++ src/data/mongo/models/product.model.ts
@@ -22,6 +22,10 @@ const productSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
+  img: {
+    type: String,
+    default: "",
+  },
   category: {
     type: Schema.Types.ObjectId,
     ref: "Category",
